52904c2d4f0a894f655e8328463633d30e3b77d9
MMakzim <98110299+MMakzim@users.noreply.github.com>
2024-04-13 16:55:56 +0300

    счет фруктов функция

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('test');
+
+const fruits = ['kiwi', 'apple', 'kiwi', 'orange', 'kiwi', 'apple'];
+
+const countItems = (list) =>{
+const count = {};
+
+fruits.forEach( f => {
+    if (!count[f]){
+        count[f] = 1;
+    } else {
+        count[f]++;
+    }
+
+});
+return count;
+};
+
+const countResult = countItems(fruits);
+console.log(countResult);
