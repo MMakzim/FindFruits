add17bef903aa71cd8777be6261d94771c108ce4
MMakzim <98110299+MMakzim@users.noreply.github.com>
2024-05-07 07:46:57 +0300

    - coment

--- script.js
+++ script.js
@@ -5,52 +5,34 @@ const fruits = ['kiwi', 'apple', 'kiwi', 'orange', 'green apple',
 
 //пошук фрукта у консолі              
 const countItems = (list) => {
-const count = {};
-fruits.forEach( f => {
-    if (!count[f]){
-        count[f] = 1;
-    } else {
-        count[f]++;
-    }
-});
-return count;
+    const count = {};
+    fruits.forEach( f => {
+        if (!count[f]){
+            count[f] = 1;
+        } else {
+            count[f]++;
+        }
+    });
+    return count;
 };
 
 const countResult = countItems(fruits);
 console.log(countResult);
 
 
-/* 
-function findFruits(){
-    
-const count = {};
-fruits.forEach( f => {
-    if (!count[f]){
-        count[f] = 1;
-    } else {
-        count[f]++;
-    }
-});
-console.log(count);
-
-}
- */
 
 let fruitName = document.getElementById("inputText");
 let printRes = document.getElementById("printRes");
 
 function fruitsDetect(){  
    
-  let res = fruits.filter((word) => word.includes(fruitName.value)); 
-    
-    if (res >= fruitName){   
+    let res = fruits.filter((word) => word.includes(fruitName.value)); 
+    if (res){   
         printRes.textContent = 'найдено ' + res;
-        }
+    }
     else {  
         printRes.textContent = 'ничего нет';
-        }  
-        
-    
+    }          
     
 }
 
